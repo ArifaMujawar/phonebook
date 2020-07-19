c5ee697cba7ccc9e2a74a57ac5a7109064879ee2
ArifaMujawar <arifabegam.mujawar@gmail.com>
2020-07-19 22:41:54 +0300

    adding production build to backend

---
+++ build/precache-manifest.3a83646fea5a97c22cb640606e13390d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "195f6c6a9a3cbd3788eec32d591265fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba244bd94f7c964de2a7",
+    "url": "/static/css/main.e7255e6b.chunk.css"
+  },
+  {
+    "revision": "688d075984a54c7c4a80",
+    "url": "/static/js/2.906cf63d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.906cf63d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba244bd94f7c964de2a7",
+    "url": "/static/js/main.feaa4ddd.chunk.js"
+  },
+  {
+    "revision": "ac2dfa88fdf6f917031a",
+    "url": "/static/js/runtime-main.34eb092b.js"
+  }
+]);
